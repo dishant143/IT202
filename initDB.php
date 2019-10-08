9548b43808b9f1f025116c77a51df49a421aa8b1
dp537 <dp537@njit.edu>
2019-10-07 23:23:04 -0400

    adding initDB.php

--- initDB.php
+++ initDB.php
@@ -1,14 +1,16 @@
 <?php
-//TODO add error handling
-
-//load the config from the same directory
-require("config.php");
-echo "Loaded host: " . $host;
-
+#turn error reporting on
+ini_set('display_errors',1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+//pull in config.php so we can access the variables from it
+require('config.php');
+echo "Loaded Host: " . $host;
+$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
 try{
-	$conn_string = "mysql:host=$host;dbname=$database;charset=utf8mb4";
 	$db = new PDO($conn_string, $username, $password);
 	echo "Connected";
+	//create table
 	$query = "create table if not exists `TestUsers`(
 		`id` int auto_increment not null,
 		`username` varchar(30) not null unique,
@@ -42,12 +44,10 @@ try{
 	$r = $stmt->execute(array(":username"=> "Billy"));
 	$results = $stmt->fetch(PDO::FETCH_ASSOC);
 	//print_r($stmt->errorInfo());
-	echo "<pre>" . var_export($results, true) . "</pre>";
+	echo "<pre>" . var_export($results, true) . "</pre>"; 
 }
 catch(Exception $e){
 	echo $e->getMessage();
-	echo "Something went wrong";
+	exit("Something went wrong");
 }
-
 ?>
-
