3af6b2241f5491726f29c5cbe3652ee0a97a1ab5
dp537 <dp537@njit.edu>
2019-11-29 22:44:10 -0500

    this is the final project

---
+++ Milestone2/tic-tac/index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Welcome to Tic Tac Toe Game Landing Page</title>
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+
+<nav class="navbar navbar-expand-sm bg-light bg-primary">
+<!-- Brand/logo -->
+<a class="navbar-brand" href="#">
+  <img src="logo.png" alt="logo" style="width:40px;">
+</a>
+
+<!-- Links -->
+<ul class="navbar-nav">
+  <li class="nav-item">
+    <a class="nav-link" href="index.php">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="register.php">Register</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="login.php">Login</a>
+  </li>
+</ul>
+</nav>
+  <div class="container" style="min-height:500px;" >
+    <h1 style="padding:293px;">Welcome to Tic Tac Toe Game<h1>
+
+  </div>
+
+  <div class="container">
+    <h2 class="text-center" style="margin-right:20px;">How the Game look Like</h2>
+    <div class="row">
+
+      <div class="col-md-4 col-sm-4 col-xs-4">
+
+      </div>
+      <div class="col-md-4 col-sm-4 col-xs-4">
+          <img src="img/iphone-mock.png" style="width:300px;height:350px"/>
+      </div>
+      <div class="col-md-4 col-sm-4 col-xs-4">
+
+      </div>
+
+    </div>
+  </div>
+
+  <div class="container" style="padding-top:30px;">
+    <h2 class="text-center">Screenshots of the Game</h2>
+    <div class="row">
+
+      <div class="col-md-4 col-sm-4 col-xs-4">
+          <img src="img/screenshots/screenshot-1.jpg" style="width:300px;height:350px"/>
+      </div>
+      <div class="col-md-4 col-sm-4 col-xs-4">
+          <img src="img/screenshots/screenshot-2.jpg" style="width:300px;height:350px"/>
+      </div>
+      <div class="col-md-4 col-sm-4 col-xs-4">
+          <img src="img/screenshots/screenshot-3.jpg" style="width:300px;height:350px"/>
+      </div>
+
+    </div>
+  </div>
+
+  </body>
+</html>
